c586f9ebb6cf53b34c60fa8547e3f22fbcc8617b
ethnvo <voethan2@gmail.com>
2025-06-24 00:37:43 -0700

    set up dependencies and wrote readme

---
+++ sleeves-ace/src/main/index.ts
@@ -0,0 +1,21 @@
+import { app, BrowserWindow } from "electron";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+
+// ESM replacements for __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      preload: join(__dirname, "preload.js"), // works now
+    },
+  });
+
+  win.loadURL("http://localhost:5173");
+};
+
+app.whenReady().then(createWindow);
